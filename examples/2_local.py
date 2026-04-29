8d8fa7c90c15058c4dbeba8daf5be2db546d3498
jahangir842 <jahangir80842@gmail.com>
2026-04-29 13:17:41 +0500

    feat: monte carlo estimation of PI example added

--- examples/2_local.py
+++ examples/2_local.py
@@ -10,7 +10,7 @@ def burn_cpu(operations):
 if __name__ == "__main__":
     ray.init() # Starts an isolated local instance
     CORES = int(ray.cluster_resources().get('CPU', 1))
-    TOTAL = 100_000_000
+    TOTAL = 1000000000
     print(f"Running on {CORES} Local Cores...")
     
     start = time.time()
